9adf74e4aee07448ff89b156d672f7395ef84e15
Srikanth <gubbasrikanth2355@gmail.com>
2020-05-27 19:04:55 +0530

    18 emojis added first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,116 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id:5,
+    emoji:"😍",
+    name:"Smiling Face with Heart-Eyes",
+    meaning:"A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing."
+  },
+  {
+    id:6,
+    emoji:"🥰",
+    name:"Smiling Face with Hearts",
+    meaning:"A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."   
+  },
+  {
+    id:7,
+    emoji:"😘",
+    name:"Face Blowing a Kiss",
+    meaning:"A yellow face winking with puckered lips blowing a kiss, depicted as a small, red heart. May represent a kiss goodbye or good night and convey feelings of love and affection more generally."
+  },
+  {
+    id:8,
+    emoji:"🤩",
+    name:"Star-Struck",
+    meaning:"A yellow face with a broad, open smile, showing upper teeth on most platforms, with stars for eyes, as if seeing a beloved celebrity. Color of stars widely varies by platform, though commonly gold or red. May express that someone or something is amazing, fascinating, impressive, or exciting."
+  },
+  {
+    id:9,
+    emoji:"😜",
+    name:"Winking Face with Tongue",
+    meaning:"A yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking."
+  },
+  {
+    id:10,
+    emoji:"🤪",
+    name:"Zany Face",
+    meaning:"A smiley making a silly face. A yellow face with a big grin and wide, white eyes, one larger than the other and in a wild, cockeyed expression. Many platforms, including Apple, depict its tongue stuck out and head tilted; others feature a full-toothed grin, giving it a more unhinged appearance."
+  },
+  {
+    id:11,
+    emoji:"😝",
+    name:"Squinting Face with Tongue",
+    meaning:"A yellow face with scrunched, X-shaped eyes and a big grin, sticking out its tongue. Often conveys a sense of fun, excitement, playfulness, hilarity, and happiness, as if saying Squee! or Awesome!"
+  },
+  {
+    id:12,
+    emoji:"🤗",
+    name:"Hugging Face",
+    meaning:"A yellow face smiling with open hands, as if giving a hug. May be used to offer thanks and support, show love and care, or express warm, positive feelings more generally."
+  },
+  {
+    id:13,
+    emoji:"🤭",
+    name:"Face with Hand Over Mouth",
+    meaning:"A yellow face with a hand covering its mouth. Apple and Facebook feature simple, open eyes, suggesting someone gasping Oh my! in serious surprise, shock, concern, or apology"
+  },
+  {
+    id:14,
+    emoji:"🤫",
+    name:"Shushing Face",
+    meaning:"A yellow face placing an index finger over pursed lips, as if issuing Shh! or Shush! for silence. Some platforms feature raised eyebrows. Apple’s design suggests the face is tilting back slightly."
+  },
+  {
+    id:15,
+    emoji:"🤔",
+    name:"Thinking Face",
+    meaning:"A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought"
+  },
+  {
+    id:16,
+    emoji:"🤐",
+    name:" Zipper-Mouth Face",
+    meaning:"A yellow face with simple, open eyes and a closed zipper for a mouth. Meaning widely varies, but commonly conveys a secret or that someone will keep one.May also be used to tell someone to stop talking"
+  },
+  {
+    id:17,
+    emoji:"🤨",
+    name:"Face with Raised Eyebrow",
+    meaning:"A yellow face with a flat, neutral mouth and furrowed eyebrows, its left raised higher than its right. Conveys a wide variety of sentiments, including suspicion, skepticism, concern, consideration, disbelief, and disapproval"
+  },
+  {
+    id:18,
+    emoji:"😐",
+    name:"Neutral Face",
+    meaning:"A yellow face with simple, open eyes and a flat, closed mouth. Intended to depict a neutral sentiment but often used to convey mild irritation and concern or a deadpan sense of humor."
+  }
+];
+
+export default emojipedia;
