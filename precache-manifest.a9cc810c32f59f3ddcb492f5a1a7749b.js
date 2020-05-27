d2fa56d53c4f94351730428032aa5f32abfbcc11
Srikanth <gubbasrikanth2355@gmail.com>
2020-05-27 19:05:57 +0530

    Updates

---
+++ precache-manifest.a9cc810c32f59f3ddcb492f5a1a7749b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8e72c683e8fb8eac10e0b870b44f1c",
+    "url": "/emojispedia/index.html"
+  },
+  {
+    "revision": "aeb5e3fdcbbede739722",
+    "url": "/emojispedia/static/js/2.3dd3d7e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emojispedia/static/js/2.3dd3d7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13619ed224ac77431291",
+    "url": "/emojispedia/static/js/main.ce40c3fc.chunk.js"
+  },
+  {
+    "revision": "5bd196e1748b0d4758f6",
+    "url": "/emojispedia/static/js/runtime-main.d8e5d9ee.js"
+  }
+]);
